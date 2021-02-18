4552a54b498cb0909eb0233786f7e1dd2758159f
Seonghwan Lee <uplsh580@gmail.com>
2021-02-18 12:05:45 +0000

    sample problem

---
+++ script/sample.py
@@ -0,0 +1,15 @@
+# https://www.acmicpc.net/problem/4344
+
+def solution(n, score):
+    avg_score = sum(score)/n
+    over_s = sum(1 for x in score if x > avg_score)
+    return round((over_s/n) * 100,3)
+
+if __name__ == '__main__':
+    c = int(input())
+    for i in range(c):
+        args = list(map(int, input().split()))
+        n = args[0]
+        score = args[1:]
+
+        print("{:.3f}%".format(solution(n, score)))
