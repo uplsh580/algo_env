25bd1e2f5da23214be2d844c9443395dca4b476e
Seonghwan Lee <uplsh580@gmail.com>
2021-02-21 23:56:55 +0000

    Selection Sort Problems

---
+++ script/selection_sort_practice.py
@@ -0,0 +1,15 @@
+# Ploblem : https://www.acmicpc.net/problem/11399
+# [Required] Use the Selection Sort to solve it.
+
+# Solver : 
+# Solved Date : 
+# BigO:
+
+def sol(n_list):
+    total_time = 0
+    return total_time
+
+if __name__ == '__main__':
+    n = int(input())
+    n_list = list(map(int,input().split()))
+    print(sol(n_list))
---
+++ script/selection_sort_simple.py
@@ -0,0 +1,14 @@
+# https://www.acmicpc.net/problem/2752
+# [Required] Use the Selection Sort to solve it.
+
+# Solver : 
+# Solved Date : 
+# BigO:
+
+
+def selection_sort(n_list):
+    return n_list
+
+if __name__ == '__main__':
+    n_list = list(map(int,input().split()))
+    print(' '.join(map(str,selection_sort(n_list))))
