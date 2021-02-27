f50c09a92cdf329003d4103079a548be468e4982
Seonghwan Lee <uplsh580@gmail.com>
2021-02-27 01:20:28 +0000

    Problems: Maximum_Subsequence_Sum Series

---
+++ script/mss.py
@@ -0,0 +1,13 @@
+# Ploblem : [연속합] https://www.acmicpc.net/problem/1912
+
+# Solver : 
+# Solved Date : 
+# BigO:
+
+def mss(n, seq):
+    return 0
+
+if __name__ == '__main__':
+    n = int(input())
+    seq = list(map(int, input().split()))
+    print(mss(n, seq))
