649106e9d0fa848094d29a2c345d9546601b02fb
Seonghwan Lee <uplsh580@gmail.com>
2021-03-08 00:16:46 +0000

    Problems: Fibonacci Series (CPP)

---
+++ script/fibonacci_level2.cpp
@@ -0,0 +1,19 @@
+// Ploblem: [피보나치 수 4] https://www.acmicpc.net/problem/10826
+
+// Solver: 
+// Solved Date: 
+// BigO:
+
+#include<iostream>
+using namespace std;
+
+int fib(int n){
+    return 0;
+}
+
+int main(){
+    int n;
+    scanf("%d", &n);
+    printf("%d", fib(n));
+    return 0;
+}
---
+++ script/fibonacci_level3.cpp
@@ -0,0 +1,19 @@
+// Ploblem: [피보나치 수 3] https://www.acmicpc.net/problem/2749
+
+// Solver: 
+// Solved Date: 
+// BigO:
+
+#include<iostream>
+using namespace std;
+
+int fib(int n){
+    return 0;
+}
+
+int main(){
+    int n;
+    scanf("%d", &n);
+    printf("%d", fib(n));
+    return 0;
+}
